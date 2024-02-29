78ca0a58f8bf8ace1b930da3389f0e16983140f5
shaikriyaz0293 <150535895+shaikriyaz0293@users.noreply.github.com>
2024-02-29 16:43:28 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello riyaz devops ");
-      System.out.println("Hello riyaz  ");
+      System.out.println("Hello riyaz ur code is ruuning know u write new code  ");
     }  
 } 
